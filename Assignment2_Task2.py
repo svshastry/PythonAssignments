eba04d46cdb8e03f8953e8051d0b3598e79c4513
svshastry <svshastry@gmail.com>
2025-12-11 03:08:13 +0530

    Assignments

---
+++ Assignment2_Task2.py
@@ -0,0 +1,26 @@
+"""
+Task 1: Check if a Number is Even or Odd
+Problem Statement: Write a Python program that:
+1. Takes an integer input from the user.
+2. Checks whether the number is even or odd using an if-else statement.
+3. Displays the result accordingly.
+
+Expected Output:
+
+The program should return an output like:
+enter a number : 7
+7 is an odd number
+enter a number : 12
+12 is an even number.
+"""
+
+
+a = input("Enter a first name: ", )
+
+
+
+c = a + " " + b
+d = "Hello, " + c + "! Welcome to our program."
+print(d)
+
+
