0134c66f4d6521c1ecad4604730fee71d990abaf
svshastry <svshastry@gmail.com>
2025-12-11 03:14:14 +0530

    Assignments Task 2 1

--- Assignment2_Task1.py
+++ Assignment2_Task1.py
@@ -1,25 +1,24 @@
 """
-    Task 1: Perform Basic Mathematical Operations
-Problem Statement: Write a Python program that does the following:
-1.  Takes two numbers as input from the user.
-2.  Performs the basic mathematical operations on these two numbers:
-o	Addition
-o	Subtraction
-o	Multiplication
-o	Division
-3.  Displays the results of each operation on the screen.
- Expected Output:
-The output should include the result of each operation performed, for example:
-numeral format = int, float
+Task 1:
+
+Check if a Number is Even or Odd Problem Statement: Write a Python program that: 1. Takes an integer input from the user.
+2. Checks whether the number is even or odd using an if-else statement.
+3. Displays the result accordingly.
+
+Expected Output:
+The program should return an output like:
+enter a number : 7
+7 is an odd number
+enter a number : 12
+12 is an even number.
 """
 
-a = input("Enter your first number: ", )
-b = input("Enter your second number: ", )
-c = float(a) + float(b)
-d = float(a) - float(b)
-e = float(a) * float(b)
-f = float(a) / float(b)
-print("Addition ", "=", c)
-print("Subtraction ", "=", d)
-print("Multiplication ", "=", e)
-print("Division ", "=", f)
+num = int(input("enter a number : "))
+
+# Check if the number is even or odd
+# For even numbers, the remainder when divided by 2 is 0
+
+if num % 2 == 0:
+    print(num, "is an even number")
+else:
+    print(num, "is an odd number")
