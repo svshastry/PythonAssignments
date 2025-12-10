822f3e098dbe848a9e7b15066ccc5f84389c84c6
svshastry <svshastry@gmail.com>
2025-12-11 03:49:19 +0530

    Assignments Task 2 1

--- Assignment2_Task2.py
+++ Assignment2_Task2.py
@@ -1,26 +1,18 @@
 """
-Task 1: Check if a Number is Even or Odd
-Problem Statement: Write a Python program that:
-1. Takes an integer input from the user.
-2. Checks whether the number is even or odd using an if-else statement.
-3. Displays the result accordingly.
+Task 2: Check if a Number is Even or Odd
+Write a Python program that:
+1.   Uses a for loop to iterate over numbers from 1 to 50.
+2.   Calculates the sum of all integers in this range.
+3.   Displays the final sum.
 
-Expected Output:
+Expected output:
 
-The program should return an output like:
-enter a number : 7
-7 is an odd number
-enter a number : 12
-12 is an even number.
+The sum of numbers from 1 to 50 is: 1275
+
 """
 
-
-a = input("Enter a first name: ", )
-
-
-
-c = a + " " + b
-d = "Hello, " + c + "! Welcome to our program."
-print(d)
-
+sum = 0
+for i in range(1, 51):
+    sum += i
+print("The sum of numbers from 1 to 50 is: ", sum)
 
